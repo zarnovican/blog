e0798d8aab13940d065443148d9757f150b2f2e4
Brano Zarnovican <zarnovican@gmail.com>
2015-11-06 17:25:42 +0100

    social? what social?

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,8 +26,10 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('zarnovican@github', 'http://github.com/zarnovican'),
+    ('zarnovican@linkedin', 'http://cz.linkedin.com/in/zarnovican'),
+)
 
 DEFAULT_PAGINATION = 10
 
